e0ed9fef573d9aa0e911d88556273bcbc8b47606
ALiang <coder_wclong@163.com>
2022-05-13 18:00:41 +0800

    Supply and fix the test cases

---
+++ test/test_util.py
@@ -0,0 +1,24 @@
+import random
+import string
+import time
+from math import floor
+
+
+def current_sec_ts():
+    return int(floor(time.time()))
+
+
+def current_msec_ts():
+    return int(floor(time.time() * 1000))
+
+
+def sec_ts_after_five_secs():
+    return current_sec_ts() + 5
+
+
+def msec_ts_after_five_secs():
+    return current_sec_ts() + 5000
+
+
+def random_string(n):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(n))
